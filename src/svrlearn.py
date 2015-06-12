0d773b1de0ef0d97950315f01993b69a469771ae
icymorn <moevery@gmail.com>
2015-06-12 19:03:09 +0800

    svr chart

--- src/svrlearn.py
+++ src/svrlearn.py
@@ -12,14 +12,24 @@ print __doc__
 ###############################################################################
 # Generate sample data
 import numpy as np
-  
-X = np.array([10, 20, 30, 40, 50, 60, 90])# np.sort(5*np.random.rand(40, 1), axis=0)
+
+
+def chart(data):
+    x_lines = [p[0] for p in data]
+    y_lines = [p[1] for p in data]
+
+x_base = [x for x in range(90)]
+
+X = np.sort(np.matrix(x_base), axis=0)
 y = np.sin(X).ravel()
-  
+
+print y
 ###############################################################################
 # Add noise to targets
 y[::5] += 3*(0.5 - np.random.rand(8))
-  
+
+print y
+
 ###############################################################################
 # Fit regression model
 from sklearn.svm import SVR
@@ -45,3 +55,6 @@ pl.ylabel('target')
 pl.title('Support Vector Regression')
 pl.legend()
 pl.show()
+
+if __name__ == '__main__':
+    chart([(0, 0.1),(5, 0.15),(10,0.3),(15,0.4),(20,0.5),(25,0.65),(30,0.8),(35,0.90),(40,0.95),(45,0.97),(50,0.99),(55,0.995),(60,0.9995)])
